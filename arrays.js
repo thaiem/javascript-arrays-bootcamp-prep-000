b2ac1bd14b5746ea86b50549ca7813429f123b0c
Elaine Thai <ethai1827@gmail.com>
2019-05-11 00:21:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,7 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.push()
 }
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
